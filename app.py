3daef21a4050529af0a4d0a46cbc9e7606df3451
Parshuram Singh <parshuramsingh433@gmail.com>
2024-07-25 22:27:53 +0530

    third commit

---
+++ app.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+
+    # print(a+b)
+    return a+b
+
+
+a = int(input("entern the first no :"))
+b = int(input("entern the second no : "))
+
+# addition(a,b)
+print(addition(a,b))
