b4116ab98826e835a492d402b994c7872243ab85
Parshuram Singh <parshuramsingh433@gmail.com>
2024-07-26 19:55:22 +0530

    developera story

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ def substraction(a,b):
 
     return a-b
 
+def devision(a,b):
+
+    return a/b
+
 a = int(input("entern the first no :"))
 b = int(input("entern the second no : "))
 
 print(addition(a,b))
 print(substraction(a,b))
+print(devision(a,b))
