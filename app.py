ab97c137345c85aeee23a577aaabdaa3c9d08fa2
Parshuram Singh <parshuramsingh433@gmail.com>
2024-07-26 19:41:02 +0530

    updated git ignore

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 def addition(a,b):
 
-    # print(a+b)
     return a+b
 
+def substraction(a,b):
+
+    return a-b
 
 a = int(input("entern the first no :"))
 b = int(input("entern the second no : "))
 
-# addition(a,b)
 print(addition(a,b))
+print(substraction(a,b))
